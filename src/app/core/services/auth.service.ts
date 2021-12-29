d7f6629bd667e06c0147c4ebb9d000b87486d1b1
Duncan <dmcphers@cardsortsoftwares.net>
2021-12-29 08:28:27 -0700

    added sign in and up methods

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../app.module";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  public signInWithEmail(email: string, password: string): void {
+    void signInWithEmailAndPassword(auth, email, password);
+  }
+
+  public signInWithGoogle(): void {
+    const provider = new GoogleAuthProvider();
+    void signInWithPopup(auth, provider);
+  }
+
+  public signUpWithEmail(email: string, password: string): void {
+    void createUserWithEmailAndPassword(auth, email, password);
+  }
 }
