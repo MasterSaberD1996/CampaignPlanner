4ef745d6327549fd0643f2fd7533f80d6517719c
Duncan <dmcphers@1800contacts.com>
2022-01-07 10:20:56 -0700

    cleaned up from test files

--- src/app/animated-label.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimatedLabelDirective } from './animated-label.directive';
-
-describe('AnimatedLabelDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AnimatedLabelDirective();
-    expect(directive).toBeTruthy();
-  });
-});
